84b35768a42df268d548ac603016f9486fd43ab0
Souher Al-Taleb <altalebsouher8@gmail.com>
2026-08-07 22:33:22 +0200

    add folders and rules

---
+++ src/components/Header.tsx
@@ -0,0 +1,17 @@
+function Header() {
+  return (
+    <header className="px-6 py-8 text-center">
+      <p className="mb-2 text-sm tracking-[0.3em] text-kamen-gold uppercase">
+        Italian Restaurant
+      </p>
+
+      <h1 className=" text-4xl font-semibold tracking-wide text-kamen-gold">
+        Kamen Pizza
+      </h1>
+
+      <p className="mt-3 text-sm text-kamen-cream">Pizza made with passion</p>
+    </header>
+  );
+}
+
+export default Header;
